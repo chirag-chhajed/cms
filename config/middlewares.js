822be0e919be3e5c70acd0692b26e51821f94a99
Amarjeet Sarma <38986305+amarjeet987@users.noreply.github.com>
2023-03-11 16:58:41 +0530

    added proper directives

--- config/middlewares.js
+++ config/middlewares.js
@@ -12,14 +12,18 @@ module.exports = [
             "data:",
             "blob:",
             "dl.airtable.com",
-            "wanderrides.s3.ap-south-1.amazonaws.com",
+            `https://${env("AWS_BUCKET")}.s3.${env(
+              "AWS_REGION"
+            )}.amazonaws.com/`,
           ],
           "media-src": [
             "'self'",
             "data:",
             "blob:",
             "dl.airtable.com",
-            "wanderrides-content.s3.ap-south-1.amazonaws.com",
+            `https://${env("AWS_BUCKET")}.s3.${env(
+              "AWS_REGION"
+            )}.amazonaws.com/`,
           ],
           upgradeInsecureRequests: null,
         },
