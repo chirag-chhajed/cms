ff78f9f221fec3f418d1f7df9d243bb98933cbb1
Amarjeet Sarma <38986305+amarjeet987@users.noreply.github.com>
2023-03-11 12:35:32 +0530

    initial commit

---
+++ config/database.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.exports = ({ env }) => {
+  return {
+    connection: {
+      client: "postgres",
+      connection: {
+        host: env("DATABASE_HOST", "127.0.0.1"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "content-database"),
+        user: env("DATABASE_USERNAME", "postgres"),
+        password: env("DATABASE_PASSWORD", "#Teambringit987"),
+      },
+      useNullAsDefault: true,
+    },
+  };
+};
