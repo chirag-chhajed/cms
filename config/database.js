48c31a16d6d93be96ebd1dbb06b2247477b96ae8
Amarjeet Sarma <38986305+amarjeet987@users.noreply.github.com>
2023-03-11 14:31:29 +0530

    added db name

--- config/database.js
+++ config/database.js
@@ -10,7 +10,7 @@ module.exports = ({ env }) => {
           "content-database.coch3fcuahkn.ap-south-1.rds.amazonaws.com"
         ),
         port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "content-database"),
+        database: env("DATABASE_NAME", "postgres"),
         user: env("DATABASE_USERNAME", "postgres"),
         password: env("DATABASE_PASSWORD", "#Teambringit123"),
       },
