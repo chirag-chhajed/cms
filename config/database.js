28b6385966a42cc4f9c16b79b4234841ab63a881
Amarjeet Sarma <38986305+amarjeet987@users.noreply.github.com>
2023-03-11 13:55:25 +0530

    updated files for db and stuff

--- config/database.js
+++ config/database.js
@@ -9,7 +9,7 @@ module.exports = ({ env }) => {
         port: env.int("DATABASE_PORT", 5432),
         database: env("DATABASE_NAME", "content-database"),
         user: env("DATABASE_USERNAME", "postgres"),
-        password: env("DATABASE_PASSWORD", "#Teambringit987"),
+        password: env("DATABASE_PASSWORD", "#Teambringit123"),
       },
       useNullAsDefault: true,
     },
--- config/plugin.js
+++ config/plugin.js
@@ -3,11 +3,11 @@ module.exports = ({ env }) => ({
     config: {
       provider: "aws-s3",
       providerOptions: {
-        accessKeyId: env("AKIAUGZ6N7NUHE4RR5HZ"),
-        secretAccessKey: env("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"),
-        region: env("ap-south-1"),
+        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+        secretAccessKey: env("AWS_ACCESS_SECRET"),
+        region: env("AWS_REGION"),
         params: {
-          Bucket: env("wanderrides-content"),
+          Bucket: env("AWS_BUCKET_NAME"),
         },
       },
       // These parameters could solve issues with ACL public-read access — see [this issue](https://github.com/strapi/strapi/issues/5868) for details
