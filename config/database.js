19fa352d1aca4936aa36d573e5e4f29baa0d5a36
Amarjeet Sarma <38986305+amarjeet987@users.noreply.github.com>
2023-03-11 14:17:34 +0530

    added db host

--- config/database.js
+++ config/database.js
@@ -5,7 +5,10 @@ module.exports = ({ env }) => {
     connection: {
       client: "postgres",
       connection: {
-        host: env("DATABASE_HOST", "127.0.0.1"),
+        host: env(
+          "DATABASE_HOST",
+          "content-database.coch3fcuahkn.ap-south-1.rds.amazonaws.com"
+        ),
         port: env.int("DATABASE_PORT", 5432),
         database: env("DATABASE_NAME", "content-database"),
         user: env("DATABASE_USERNAME", "postgres"),
