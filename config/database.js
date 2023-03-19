48f96ad0dafd375b96d36eed98fb93254869cbfa
Amarjeet Sarma <38986305+amarjeet987@users.noreply.github.com>
2023-03-19 20:03:04 +0530

    added blog collection

--- config/database.js
+++ config/database.js
@@ -1,18 +1,13 @@
-const path = require("path");
-
 module.exports = ({ env }) => {
   return {
     connection: {
       client: "postgres",
       connection: {
-        host: env(
-          "DATABASE_HOST",
-          "content-database.coch3fcuahkn.ap-south-1.rds.amazonaws.com"
-        ),
+        host: env("DATABASE_HOST", "localhost"),
         port: env.int("DATABASE_PORT", 5432),
         database: env("DATABASE_NAME", "postgres"),
         user: env("DATABASE_USERNAME", "postgres"),
-        password: env("DATABASE_PASSWORD", "#Teambringit123"),
+        password: env("DATABASE_PASSWORD", "teambringit123"),
       },
       useNullAsDefault: true,
     },
